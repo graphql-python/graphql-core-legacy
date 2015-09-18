fa2aa3136ff0667ae92a24ba7c13e7424ead20fa
rawls238 <garidor@bu.edu>
2015-09-18 00:30:41 -0400

    add unique argument names

--- graphql/core/validation/rules.py
+++ graphql/core/validation/rules.py
@@ -256,7 +256,29 @@ class KnownArgumentNames(ValidationRule):
 
 
 class UniqueArgumentNames(ValidationRule):
-    pass
+    def __init__(self, context):
+        super(UniqueArgumentNames, self).__init__(context)
+        self.known_arg_names = {}
+
+    def enter_Field(self, node, *args):
+        self.known_arg_names = {}
+
+    def enter_Directive(self, node, key, parent, path, ancestors):
+        self.known_arg_names = {}
+
+    def enter_Argument(self, node, *args):
+        arg_name = node.name.value
+        if arg_name in self.known_arg_names:
+            return GraphQLError(
+                self.duplicate_arg_message(arg_name),
+                [self.known_arg_names[arg_name], node.name]
+            )
+        self.known_arg_names[arg_name] = node.name
+
+    @staticmethod
+    def duplicate_arg_message(field):
+        return 'There can only be one argument named {}'.format(field)
+
 
 
 class ArgumentsOfCorrectType(ValidationRule):
