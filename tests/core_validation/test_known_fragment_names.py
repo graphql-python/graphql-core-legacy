f51a269b4667a42a47ff36bf4cc6189c5fd32f89
Jake <me@jh.gg>
2015-09-18 13:46:27 -0400

    Implement KnownFragmentNames
    - implement unit tests
    - fix ValidationContext.get_fragment to use self.get_ast() instead of self.get_document()

---
+++ tests/core_validation/test_known_fragment_names.py
@@ -0,0 +1,55 @@
+from graphql.core.language.location import SourceLocation
+from graphql.core.validation.rules import KnownFragmentNames
+from utils import expect_passes_rule, expect_fails_rule
+
+
+def undefined_fragment(fragment_name, line, column):
+    return {
+        'message': KnownFragmentNames.unknown_fragment_message(fragment_name),
+        'locations': [SourceLocation(line, column)]
+    }
+
+
+def test_known_fragment_names_are_valid():
+    expect_passes_rule(KnownFragmentNames, '''
+    {
+        human(id: 4) {
+            ...HumanFields1
+            ... on Human {
+                ...HumanFields2
+            }
+        }
+    }
+    fragment HumanFields1 on Human {
+        name
+        ...HumanFields3
+    }
+    fragment HumanFields2 on Human {
+        name
+    }
+    fragment HumanFields3 on Human {
+        name
+    }
+    ''')
+
+
+def test_unknown_fragment_names_are_invalid():
+    expect_fails_rule(KnownFragmentNames, '''
+    {
+        human(id: 4) {
+            ...UnknownFragment1
+            ... on Human {
+                ...UnknownFragment2
+            }
+        }
+    }
+    fragment HumanFields on Human {
+        name
+        ...UnknownFragment3
+    }
+    ''', [
+        undefined_fragment('UnknownFragment1', 4, 16),
+        undefined_fragment('UnknownFragment2', 6, 20),
+        undefined_fragment('UnknownFragment3', 12, 12),
+
+    ])
