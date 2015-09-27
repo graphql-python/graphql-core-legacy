d28b617f16e1f178461129e2189817da5cf8123c
Taeho Kim <dittos@gmail.com>
2015-09-27 14:49:11 +0900

    Capture parsing error and wrap in ExecutionResult (fix #53)

--- graphql/core/__init__.py
+++ graphql/core/__init__.py
@@ -5,18 +5,24 @@ from .validation import validate
 
 
 def graphql(schema, request='', root=None, vars=None, operation_name=None):
-    source = Source(request, 'GraphQL request')
-    ast = parse(source)
-    validation_errors = validate(schema, ast)
-    if validation_errors:
+    try:
+        source = Source(request, 'GraphQL request')
+        ast = parse(source)
+        validation_errors = validate(schema, ast)
+        if validation_errors:
+            return ExecutionResult(
+                errors=validation_errors,
+                invalid=True,
+            )
+        return execute(
+            schema,
+            root or object(),
+            ast,
+            operation_name,
+            vars or {},
+        )
+    except Exception as e:
         return ExecutionResult(
-            errors=validation_errors,
+            errors=[e],
             invalid=True,
         )
-    return execute(
-        schema,
-        root or object(),
-        ast,
-        operation_name,
-        vars or {},
-    )
