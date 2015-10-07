32966985301cf6b34dc8edc64358a39c3bad99df
Jake <me@jh.gg>
2015-10-07 17:26:50 -0400

    Update introspection test for new introspection query.

--- graphql/core/utils/introspection_query.py
+++ graphql/core/utils/introspection_query.py
@@ -1,62 +1,65 @@
 introspection_query = '''
-    query IntrospectionQuery {
-        __schema {
-            queryType { name }
-            mutationType { name }
-            types {
-                ...FullType
-            }
-            directives {
-                name
-                description
-                args {
-                    ...InputValue
-                }
-                onOperation
-                onFragment
-                onField
-            }
+query IntrospectionQuery {
+    __schema {
+        queryType { name }
+        mutationType { name }
+        types {
+            ...FullType
         }
-    }
-    fragment FullType on __Type {
-        kind
-        name
-        description
-        fields {
+        directives {
             name
             description
             args {
                 ...InputValue
             }
-            type {
-                ...TypeRef
-            }
-            isDeprecated
-            deprecationReason
-        }
-        inputFields {
-            ...InputValue
-        }
-        interfaces {
-            ...TypeRef
-        }
-        enumValues {
-            name
-            description
-            isDeprecated
-            deprecationReason
-        }
-        possibleTypes {
-            ...TypeRef
+            onOperation
+            onFragment
+            onField
         }
     }
-    fragment InputValue on __InputValue {
+}
+fragment FullType on __Type {
+    kind
+    name
+    description
+    fields {
         name
         description
-        type { ...TypeRef }
-        defaultValue
+        args {
+            ...InputValue
+        }
+        type {
+            ...TypeRef
+        }
+        isDeprecated
+        deprecationReason
     }
-    fragment TypeRef on __Type {
+    inputFields {
+        ...InputValue
+    }
+    interfaces {
+        ...TypeRef
+    }
+    enumValues {
+        name
+        description
+        isDeprecated
+        deprecationReason
+    }
+    possibleTypes {
+        ...TypeRef
+    }
+}
+fragment InputValue on __InputValue {
+    name
+    description
+    type { ...TypeRef }
+    defaultValue
+}
+fragment TypeRef on __Type {
+    kind
+    name
+    ofType {
         kind
         name
         ofType {
@@ -65,11 +68,8 @@ introspection_query = '''
             ofType {
                 kind
                 name
-                ofType {
-                    kind
-                    name
-                }
             }
         }
     }
+}
 '''
