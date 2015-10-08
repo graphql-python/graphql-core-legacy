f8849792366428f2536fe4fb6afa5c4823b64739
Jake <me@jh.gg>
2015-10-08 16:26:40 -0400

    [v0.1a2] Update version for newest release.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='graphql-core',
-    version='0.1a1',
+    version='0.1a2',
     description='GraphQL implementation for Python',
 
     url='https://github.com/graphql-python/graphql-core',
