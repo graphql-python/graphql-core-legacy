7a10762af2b08c8b015d648ad62345a4c6749aeb
Jake <me@jh.gg>
2015-10-26 15:44:20 -0400

    [v0.4.7b1] Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='graphql-core',
-    version='0.4.7b0',
+    version='0.4.7b1',
     description='GraphQL implementation for Python',
     url='https://github.com/graphql-python/graphql-core',
     download_url='https://github.com/graphql-python/graphql-core/releases',
