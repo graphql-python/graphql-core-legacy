1dc8c87d831deec0d4e43930ce840dc9971bad68
Syrus Akbary <me@syrusakbary.com>
2016-04-23 21:26:16 -0700

    Added pool to Thread executor

--- graphql/execution/executors/thread.py
+++ graphql/execution/executors/thread.py
@@ -1,3 +1,4 @@
+from multiprocessing.pool import ThreadPool
 from threading import Thread
 
 from ...pyutils.aplus import Promise
@@ -6,16 +7,28 @@ from .utils import process
 
 class ThreadExecutor(object):
 
-    def __init__(self):
+    pool = None
+
+    def __init__(self, pool=False):
         self.threads = []
+        if pool:
+            self.execute = self.execute_in_pool
+            self.pool = ThreadPool(processes=pool)
+        else:
+            self.execute = self.execute_in_thread
 
     def wait_until_finished(self):
         for thread in self.threads:
             thread.join()
 
-    def execute(self, fn, *args, **kwargs):
+    def execute_in_thread(self, fn, *args, **kwargs):
         promise = Promise()
         thread = Thread(target=process, args=(promise, fn, args, kwargs))
         thread.start()
         self.threads.append(thread)
+        return promise
+
+    def execute_in_pool(self, fn, *args, **kwargs):
+        promise = Promise()
+        self.pool.map(lambda input: process(*input), [(promise, fn, args, kwargs)])
         return promise
