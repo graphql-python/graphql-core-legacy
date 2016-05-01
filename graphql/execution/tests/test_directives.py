c4c618c488ce0abde982c5f7b85ce54acceaa95b
Syrus Akbary <me@syrusakbary.com>
2016-05-01 10:15:53 -0700

    Remove non spec compliant directives test
    Related GraphQL-js commits: https://github.com/graphql/graphql-js/commit/07e627adda2b236e720ec352b21eb3043170c60f

--- graphql/execution/tests/test_directives.py
+++ graphql/execution/tests/test_directives.py
@@ -181,51 +181,6 @@ def test_skip_true_omits_inline_fragment():
     assert result.data == {'a': 'a'}
 
 
-def test_if_false_omits_fragment():
-    q = '''
-        query Q {
-          a
-          ...Frag
-        }
-        fragment Frag on TestType @include(if: false) {
-          b
-        }
-    '''
-    result = execute_test_query(q)
-    assert not result.errors
-    assert result.data == {'a': 'a'}
-
-
-def test_if_true_includes_fragment():
-    q = '''
-        query Q {
-          a
-          ...Frag
-        }
-        fragment Frag on TestType @include(if: true) {
-          b
-        }
-    '''
-    result = execute_test_query(q)
-    assert not result.errors
-    assert result.data == {'a': 'a', 'b': 'b'}
-
-
-def test_skip_false_includes_fragment():
-    q = '''
-        query Q {
-          a
-          ...Frag
-        }
-        fragment Frag on TestType @skip(if: false) {
-          b
-        }
-    '''
-    result = execute_test_query(q)
-    assert not result.errors
-    assert result.data == {'a': 'a', 'b': 'b'}
-
-
 def test_skip_true_omits_fragment():
     q = '''
         query Q {
