1b602dd7b74ed8df60d984cfd17da39cc897983a
Syrus Akbary <me@syrusakbary.com>
2016-05-03 20:56:04 -0700

    Simplified Asyncio executor

--- graphql/execution/executors/asyncio.py
+++ graphql/execution/executors/asyncio.py
@@ -1,18 +1,6 @@
 from __future__ import absolute_import
 
 from asyncio import Future, ensure_future, get_event_loop, iscoroutine, wait
-from promise import Promise
-
-
-def process_future_result(promise):
-    def handle_future_result(future):
-        exception = future.exception()
-        if exception:
-            promise.reject(exception)
-        else:
-            promise.fulfill(future.result())
-
-    return handle_future_result
 
 
 class AsyncioExecutor(object):
@@ -27,9 +15,7 @@ class AsyncioExecutor(object):
     def execute(self, fn, *args, **kwargs):
         result = fn(*args, **kwargs)
         if isinstance(result, Future) or iscoroutine(result):
-            promise = Promise()
             future = ensure_future(result)
             self.futures.append(future)
-            future.add_done_callback(process_future_result(promise))
-            return promise
+            return future
         return result
