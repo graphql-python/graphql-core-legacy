a0fe92cbc8bc69bbeb3d90c3550be1440cb79fa3
Syrus Akbary <me@syrusakbary.com>
2016-08-18 23:22:21 -0700

    Improved benchmark tests

--- graphql/execution/tests/test_benchmark.py
+++ graphql/execution/tests/test_benchmark.py
@@ -4,7 +4,7 @@ from graphql import GraphQLObjectType, GraphQLField, GraphQLList, GraphQLInt, Gr
 
 
 def test_big_list_of_ints(benchmark):
-    big_int_list = [x for x in range(20000)]
+    big_int_list = [x for x in range(10000)]
 
     def resolve_all_ints(root, args, context, info):
         return big_int_list
@@ -25,7 +25,7 @@ def test_big_list_of_ints(benchmark):
     assert result.data == {'allInts': big_int_list}
 
 
-def test_big_list_of_containers_with_multiple_fields(benchmark):
+def test_big_list_of_containers_with_one_field(benchmark):
     Container = namedtuple('Container', 'x y z o')
 
     ContainerType = GraphQLObjectType('Container', fields={
@@ -35,7 +35,7 @@ def test_big_list_of_containers_with_multiple_fields(benchmark):
         'o': GraphQLField(GraphQLInt),
     })
 
-    big_container_list = [Container(x=x, y=x, z=x, o=x) for x in range(20000)]
+    big_container_list = [Container(x=x, y=x, z=x, o=x) for x in range(10000)]
 
     def resolve_all_containers(root, args, context, info):
         return big_container_list
@@ -47,10 +47,41 @@ def test_big_list_of_containers_with_multiple_fields(benchmark):
         )
     })
     hello_schema = GraphQLSchema(Query)
-    source = Source('{ allContainers { x, y, z, o } }')
+    source = Source('{ allContainers { x } }')
     ast = parse(source)
     big_list_query = partial(execute, hello_schema, ast)
     result = benchmark(big_list_query)
     # result = big_list_query()
     assert not result.errors
-    assert result.data == {'allContainers': [{'x': c.x, 'y': c.y, 'z': c.z, 'o': c.o} for c in big_container_list]}
+    assert result.data == {'allContainers': [{'x': c.x} for c in big_container_list]}
+
+
+def test_big_list_of_containers_with_multiple_fields(benchmark):
+    Container = namedtuple('Container', 'x y z o')
+
+    ContainerType = GraphQLObjectType('Container', fields={
+        'x': GraphQLField(GraphQLInt),
+        'y': GraphQLField(GraphQLInt),
+        'z': GraphQLField(GraphQLInt),
+        'o': GraphQLField(GraphQLInt),
+    })
+
+    big_container_list = [Container(x=x, y=x, z=x, o=x) for x in range(10000)]
+
+    def resolve_all_containers(root, args, context, info):
+        return big_container_list
+
+    Query = GraphQLObjectType('Query', fields={
+        'allContainers': GraphQLField(
+            GraphQLList(ContainerType),
+            resolver=resolve_all_containers
+        )
+    })
+    hello_schema = GraphQLSchema(Query)
+    source = Source('{ allContainers { x, y, z } }')
+    ast = parse(source)
+    big_list_query = partial(execute, hello_schema, ast)
+    result = benchmark(big_list_query)
+    # result = big_list_query()
+    assert not result.errors
+    assert result.data == {'allContainers': [{'x': c.x, 'y': c.y, 'z': c.z} for c in big_container_list]}
