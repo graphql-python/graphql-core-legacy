29ebeae490822c2a8460631ead4766383b1fffd2
Markus Padourek <markus.padourek@gmail.com>
2016-09-08 15:15:11 +0100

    fix flake8.

--- graphql/validation/rules/fields_on_correct_type.py
+++ graphql/validation/rules/fields_on_correct_type.py
@@ -3,7 +3,7 @@ from collections import Counter
 from ...error import GraphQLError
 from ...pyutils.ordereddict import OrderedDict
 from ...type.definition import (GraphQLInterfaceType, GraphQLObjectType,
-                                GraphQLUnionType, is_abstract_type)
+                                GraphQLUnionType)
 from ...utils.quoted_or_list import quoted_or_list
 from ...utils.suggestion_list import suggestion_list
 from .base import ValidationRule
