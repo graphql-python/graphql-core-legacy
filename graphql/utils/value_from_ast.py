530f015a3579f6b8297c6c1b964cb01a66bee128
Alex Prokofiev <prokofiev.ad@yandex.ru>
2016-09-22 16:39:30 +0300

    Update value_from_ast.py

--- graphql/utils/value_from_ast.py
+++ graphql/utils/value_from_ast.py
@@ -2,6 +2,8 @@ from ..language import ast
 from ..type import (GraphQLEnumType, GraphQLInputObjectType, GraphQLList,
                     GraphQLNonNull, GraphQLScalarType)
 
+Undefined = object()
+UndefinedList = [Undefined,]
 
 def value_from_ast(value_ast, type, variables=None):
     """Given a type and a value AST node known to match this type, build a
@@ -12,12 +14,12 @@ def value_from_ast(value_ast, type, variables=None):
         return value_from_ast(value_ast, type.of_type, variables)
 
     if not value_ast:
-        return None
+        return Undefined
 
     if isinstance(value_ast, ast.Variable):
         variable_name = value_ast.name.value
         if not variables or variable_name not in variables:
-            return None
+            return Undefined
 
         # Note: we're not doing any checking that this variable is correct. We're assuming that this query
         # has been validated and the variable usage here is of the correct type.
@@ -26,16 +28,17 @@ def value_from_ast(value_ast, type, variables=None):
     if isinstance(type, GraphQLList):
         item_type = type.of_type
         if isinstance(value_ast, ast.ListValue):
-            return [value_from_ast(item_ast, item_type, variables)
-                    for item_ast in value_ast.values]
+            result = [value_from_ast(item_ast, item_type, variables) for item_ast in value_ast.values]
+            return result if result != UndefinedList else Undefined
 
         else:
-            return [value_from_ast(value_ast, item_type, variables)]
+            result = [value_from_ast(value_ast, item_type, variables)]
+            return result if result != UndefinedList else Undefined
 
     if isinstance(type, GraphQLInputObjectType):
         fields = type.get_fields()
         if not isinstance(value_ast, ast.ObjectValue):
-            return None
+            return Undefined
 
         field_asts = {}
 
@@ -45,7 +48,7 @@ def value_from_ast(value_ast, type, variables=None):
         obj = {}
         for field_name, field in fields.items():
             field_ast = field_asts.get(field_name)
-            field_value_ast = None
+            field_value_ast = Undefined
 
             if field_ast:
                 field_value_ast = field_ast.value
@@ -53,10 +56,10 @@ def value_from_ast(value_ast, type, variables=None):
             field_value = value_from_ast(
                 field_value_ast, field.type, variables
             )
-            if field_value is None:
+            if field_value == Undefined and field.default_value is not None:
                 field_value = field.default_value
 
-            if field_value is not None:
+            if field_value != Undefined:
                 obj[field_name] = field_value
 
         return obj
@@ -64,4 +67,5 @@ def value_from_ast(value_ast, type, variables=None):
     assert isinstance(type, (GraphQLScalarType, GraphQLEnumType)), \
         'Must be input type'
 
-    return type.parse_literal(value_ast)
+    result = type.parse_literal(value_ast)
+    return  result if result is not None else Undefined
