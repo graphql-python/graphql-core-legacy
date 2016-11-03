d9784056850ce26e5c831bc7fecece01252e7a18
Romulo Furtado <romulo.furtado@loggi.com>
2016-11-03 12:40:06 -0200

    fix(graphql-error) unicode error message

--- graphql/error/located_error.py
+++ graphql/error/located_error.py
@@ -9,12 +9,10 @@ class GraphQLLocatedError(GraphQLError):
 
     def __init__(self, nodes, original_error=None):
         if original_error:
-            message = (
-                original_error.message.encode('utf-8')
-                if isinstance(original_error.message, unicode)
-                else str(original_error)
-            )
-
+            try:
+                message = str(original_error)
+            except UnicodeEncodeError:
+                message = original_error.message.encode('utf-8')
         else:
             message = 'An unknown error occurred.'
 
