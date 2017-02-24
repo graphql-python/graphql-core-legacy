7ff0f0287183f86c1e80b3eaf2bda4c0664a3417
Павел Грибанов <rapthead@gmail.com>
2017-02-24 16:52:28 +0500

    optional wrapping the middleware into promises

--- graphql/execution/tests/test_executor.py
+++ graphql/execution/tests/test_executor.py
@@ -8,6 +8,7 @@ from graphql.language.parser import parse
 from graphql.type import (GraphQLArgument, GraphQLBoolean, GraphQLField,
                           GraphQLInt, GraphQLList, GraphQLObjectType,
                           GraphQLSchema, GraphQLString)
+from promise import Promise
 
 
 def test_executes_arbitary_code():
@@ -607,3 +608,40 @@ def test_middleware_class():
     middlewares = MiddlewareManager(MyMiddleware())
     result = execute(GraphQLSchema(Type), doc_ast, Data(), middleware=middlewares)
     assert result.data == {'ok': 'ko', 'not_ok': 'ko_ton'}
+
+
+def test_middleware_skip_promise_wrap():
+    doc = '''{
+        ok
+        not_ok
+    }'''
+
+    class Data(object):
+
+        def ok(self):
+            return 'ok'
+
+        def not_ok(self):
+            return 'not_ok'
+
+    doc_ast = parse(doc)
+
+    Type = GraphQLObjectType('Type', {
+        'ok': GraphQLField(GraphQLString),
+        'not_ok': GraphQLField(GraphQLString),
+    })
+
+    class MyPromiseMiddleware(object):
+        def resolve(self, next, *args, **kwargs):
+            return Promise.resolve(next(*args, **kwargs))
+
+    class MyEmptyMiddleware(object):
+        def resolve(self, next, *args, **kwargs):
+            return next(*args, **kwargs)
+
+    middlewares_with_promise = MiddlewareManager(MyPromiseMiddleware(), wrap_in_promise=False)
+    middlewares_without_promise = MiddlewareManager(MyEmptyMiddleware(), wrap_in_promise=False)
+
+    result1 = execute(GraphQLSchema(Type), doc_ast, Data(), middleware=middlewares_with_promise)
+    result2 = execute(GraphQLSchema(Type), doc_ast, Data(), middleware=middlewares_without_promise)
+    assert result1.data == result2.data and result1.data == {'ok': 'ok', 'not_ok': 'not_ok'}
