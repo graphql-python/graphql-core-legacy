24be647e0cf12b0a12d858c4791f34a9bd210be3
Syrus Akbary <me@syrusakbary.com>
2018-06-06 14:03:12 -0700

    Improved middeware init speed

--- graphql/execution/middleware.py
+++ graphql/execution/middleware.py
@@ -8,11 +8,12 @@ MIDDLEWARE_RESOLVER_FUNCTION = 'resolve'
 
 
 class MiddlewareManager(object):
+    __slots__ = "middlewares", "wrap_in_promise", "_middleware_resolvers", "_cached_resolvers"
 
-    def __init__(self, *middlewares, **kwargs):
+    def __init__(self, *middlewares, wrap_in_promise=True):
         self.middlewares = middlewares
-        self.wrap_in_promise = kwargs.get('wrap_in_promise', True)
-        self._middleware_resolvers = list(get_middleware_resolvers(middlewares))
+        self.wrap_in_promise = wrap_in_promise
+        self._middleware_resolvers = list(get_middleware_resolvers(middlewares)) if middlewares else []
         self._cached_resolvers = {}
 
     def get_field_resolver(self, field_resolver):
