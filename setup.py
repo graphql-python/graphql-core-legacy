02605b1adce7b287fa9ee6beacd735882954159a
Syrus Akbary <me@syrusakbary.com>
2018-07-19 13:16:09 -0700

    Updated version to 2.1.0 (final)

--- setup.py
+++ setup.py
@@ -53,6 +53,8 @@ setup(
     name="graphql-core",
     version=version,
     description="GraphQL implementation for Python",
+    long_description=open("README.md").read(),
+    # long_description_content_type="text/markdown",
     url="https://github.com/graphql-python/graphql-core",
     download_url="https://github.com/graphql-python/graphql-core/releases",
     author="Syrus Akbary, Jake Heinz, Taeho Kim",
