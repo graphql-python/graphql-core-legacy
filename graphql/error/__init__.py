cd01c7e21c0bfd8e2b53c91f9af491a1bf0e7d07
Cody Scott <cody.scott@giatec.ca>
2019-01-31 12:03:44 -0500

    Add HandledGraphQLError that does not log an exception

--- graphql/error/__init__.py
+++ graphql/error/__init__.py
@@ -1,6 +1,13 @@
-from .base import GraphQLError
+from .base import GraphQLError, HandledGraphQLError
 from .located_error import GraphQLLocatedError
 from .syntax_error import GraphQLSyntaxError
 from .format_error import format_error
 
-__all__ = ["GraphQLError", "GraphQLLocatedError", "GraphQLSyntaxError", "format_error"]
+
+__all__ = [
+    "GraphQLError",
+    "GraphQLLocatedError",
+    "GraphQLSyntaxError",
+    "HandledGraphQLError",
+    "format_error",
+]
