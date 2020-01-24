6dc839834f7c1506d10336e19d66403334b60eb9
Christoph Zwerschke <cito@online.de>
2020-01-24 23:57:12 +0100

    Support Python 3.8 and use it for pre-commit and mypy
    This is still a bit limited because a compatible
    gevent version is not yet available for Python 3.8.

--- setup.py
+++ setup.py
@@ -70,6 +70,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: Implementation :: PyPy",
         "License :: OSI Approved :: MIT License",
         "Topic :: Database :: Front-Ends",
